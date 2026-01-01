557b6c23cb103518d6441e2b3ed8aa13f4e88478
maria-antoniak <maa343@cornell.edu>
2026-01-01 16:15:00 -0700

    integration with moderation services - might break

--- components/ModerationWrapper.tsx
+++ components/ModerationWrapper.tsx
@@ -82,10 +82,11 @@ export default function ModerationWrapper({
     );
   }
 
-  // Render with alerts/informs (no blur, but show badges)
+  // Render with alerts (no blur, but show alert badges)
+  // Note: "inform" badges are now handled by LabelBadges inline with the post
   return (
     <div>
-      {/* Alert badges */}
+      {/* Alert badges - only for serious warnings that should appear prominently */}
       {contentModeration.alerts.length > 0 && (
         <div className="mb-2 flex flex-wrap gap-1">
           {contentModeration.alerts.map((alert, i) => (
@@ -97,23 +98,6 @@ export default function ModerationWrapper({
                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
               {alert.displayName || alert.label || alert.type}
-            </span>
-          ))}
-        </div>
-      )}
-      
-      {/* Inform badges */}
-      {contentModeration.informs.length > 0 && (
-        <div className="mb-2 flex flex-wrap gap-1">
-          {contentModeration.informs.map((inform, i) => (
-            <span
-              key={i}
-              className="inline-flex items-center gap-1 px-2 py-0.5 text-xs font-medium bg-blue-100 dark:bg-blue-900/30 text-blue-700 dark:text-blue-300 rounded-full"
-            >
-              <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-              </svg>
-              {inform.displayName || inform.label || inform.type}
             </span>
           ))}
         </div>
