9ecb763c27e74af30773cade03b93318663f8206
maria-antoniak <maa343@cornell.edu>
2026-01-12 11:34:07 -0700

    adding avatar colors to profile pages

--- components/ProfileView.tsx
+++ components/ProfileView.tsx
@@ -701,6 +701,8 @@ export default function ProfileView({ did, avatar: avatarProp, displayName, hand
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
             </svg>
           </span>
+        ) : isMutual ? (
+          'Mutuals'
         ) : isFollowing ? (
           'Following'
         ) : (
